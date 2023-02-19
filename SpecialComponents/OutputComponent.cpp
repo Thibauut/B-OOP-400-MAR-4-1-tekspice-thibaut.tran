e544a281494e2bb4a96aff013e1ab08035d33685
TranThibaut <thibaut.tran@epitech.eu>
2023-02-19 12:45:32 +0000

    push fix true and false

--- SpecialComponents/OutputComponent.cpp
+++ SpecialComponents/OutputComponent.cpp
@@ -10,10 +10,10 @@
 #include "FalseComponent.hpp"
 
 nts::Tristate nts::OutputComponent::compute(std::size_t pin) {
-    AComponent *tmp = dynamic_cast<AComponent*>(this->_pins[1].first);
-    if (typeid(*tmp) == typeid(TrueComponent))
-        return nts::TRUE;
-    if (typeid(*tmp) == typeid(FalseComponent))
-        return nts::FALSE;
+    // AComponent *tmp = dynamic_cast<AComponent*>(this->_pins[1].first);
+    // if (typeid(*tmp) == typeid(TrueComponent))
+    //     return nts::TRUE;
+    // if (typeid(*tmp) == typeid(FalseComponent))
+    //     return nts::FALSE;
     return this->_pins[pin].first->compute(this->_pins[pin].second);
 }
